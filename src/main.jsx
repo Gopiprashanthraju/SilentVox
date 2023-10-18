3138df943a96f01696af4fe7149e5e1ced01bf74
gavinisumanth <sumanth_edu@outlook.com>
2023-10-18 17:13:03 +0530

    feat: routing with pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Player from "./pages/Player.jsx";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/v/:videoId",
+    element: <Player />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+]);
 import "./main.css";
+// theme switcher
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+)
+  import("./dark.scss");
+else import("./light.scss");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
