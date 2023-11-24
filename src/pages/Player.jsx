806ba63e93319f755c5980369f582edf1aae2092
Gavini Sumanth <100990200+GaviniSumanth@users.noreply.github.com>
2023-11-24 09:19:20 +0000

    feat: implement video player page

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,11 +1,32 @@
-import NavigationBar from "./../components/Navbar";
-function Player() {
+import Frame from "../components/Frame";
+import { VideoPlayer } from "../components/VideoPlayer";
+import Comments from "../components/Comments";
+import { VideoDeck } from "../components/VideoDeck";
+function Page() {
   return (
     <>
-      <NavigationBar color="light" light expand="md" />
-      <h1>Player</h1>
+      <div className="d-flex p-4">
+        <div
+          style={{
+            width: "80%",
+          }}
+        >
+          <VideoPlayer />
+          <Comments />
+        </div>
+        <VideoDeck title="Suggested" />
+      </div>
+    </>
+  );
+}
+function Home() {
+  return (
+    <>
+      <Frame>
+        <Page />
+      </Frame>
     </>
   );
 }
 
-export default Player;
+export default Home;
