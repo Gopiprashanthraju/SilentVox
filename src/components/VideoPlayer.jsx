7e97a24cde860a1db34f1c54a56a9e3b0fe1ecda
Gavini Sumanth <100990200+GaviniSumanth@users.noreply.github.com>
2023-11-25 05:29:12 +0000

    feat: add votes, watch later to video player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,24 +1,142 @@
 import ReactPlayer from "react-player";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import {
+  HourglassSplit,
+  ArrowUpSquare,
+  ArrowUpSquareFill,
+  ArrowDownSquare,
+  ArrowDownSquareFill,
+} from "react-bootstrap-icons";
+function Vote({ upVotes, downVotes,buttonStyle }) {
+  let initialUpVotes = upVotes;
+  let initialDownVotes = downVotes;
+  let [upVote, setUpVote] = useState(upVotes);
+  let [downVote, setDownVote] = useState(downVotes);
+  return (
+    <>
+      <div className="d-flex flex-row align-items-center gap-4">
+        <div className={buttonStyle || "d-flex flex-row align-items-center justify-content-center fs-2"}>
+          {upVote === initialUpVotes ? (
+            <ArrowUpSquare
+              onClick={() => {
+                if (upVote === initialUpVotes && downVote === initialDownVotes)
+                  setUpVote(upVote + 1);
+                else if (
+                  upVote > initialUpVotes &&
+                  downVote === initialDownVotes
+                )
+                  setUpVote(upVote - 1);
+              }}
+            />
+          ) : (
+            <ArrowUpSquareFill
+              onClick={() => {
+                if (upVote === initialUpVotes && downVote === initialDownVotes)
+                  setUpVote(upVote + 1);
+                else if (
+                  upVote > initialUpVotes &&
+                  downVote === initialDownVotes
+                )
+                  setUpVote(upVote - 1);
+              }}
+            />
+          )}
+          <h4 className="mx-2 my-0">{upVote}</h4>
+        </div>
+        <div className={buttonStyle || "d-flex flex-row align-items-center fs-2"}>
+          {downVote === initialDownVotes ? (
+            <ArrowDownSquare
+              onClick={() => {
+                if (upVote === initialUpVotes && downVote === initialDownVotes)
+                  setDownVote(downVote - 1);
+                else if (
+                  upVote === initialUpVotes &&
+                  downVote < initialDownVotes
+                )
+                  setDownVote(downVote + 1);
+              }}
+            />
+          ) : (
+            <ArrowDownSquareFill
+              onClick={() => {
+                if (upVote === initialUpVotes && downVote === initialDownVotes)
+                  setDownVote(downVote - 1);
+                else if (
+                  upVote === initialUpVotes &&
+                  downVote < initialDownVotes
+                )
+                  setDownVote(downVote + 1);
+              }}
+            />
+          )}
+          <h4 className="mx-2 my-0">{downVote}</h4>
+        </div>
+      </div>
+    </>
+  );
+}
+Vote.propTypes = {
+  upVotes: PropTypes.number.isRequired,
+  downVotes: PropTypes.number.isRequired,
+  buttonStyle:PropTypes.string,
+};
+Vote.defaultProps = {
+  upVotes: 0,
+  downVotes: 0,
+  buttonStyle:"d-flex flex-row align-items-center justify-content-center p-4 rounded-4 bg-dark text-white fs-4"
+};
+function Options({ views, upvotes, downvotes }) {
+  const [watchLater,setWatchLater] = useState(0);
+  const bStyle =
+    "d-flex flex-row align-items-center p-3 rounded-4 bg-dark text-white btn btn-lg fs-4";
+  return (
+    <div className="fs-2 d-flex align-items-center text-white p-4  justify-content-between">
+      <h5 className="fs-3 mx-1">Views: {views}</h5>
+      <div className="d-flex align-items-center gap-4">
+        <button className={bStyle} onClick={() => {
+          setWatchLater(!watchLater)
+        }}>
+          <HourglassSplit className="mx-2 my-0"/>
+          <h5 className="fs-4 mx-1">{watchLater?"Added to Watch Later":"Watch Later"}</h5>
+        </button>
+        <Vote upVotes={upvotes} downVotes={downvotes} />
+      </div>
+    </div>
+  );
+}
+
+Options.propTypes = {
+  views: PropTypes.number.isRequired,
+  upvotes: PropTypes.number.isRequired,
+  downvotes: PropTypes.number.isRequired,
+};
+Options.defaultProps = {
+  views: 0,
+  upvotes: 0,
+  downvotes: 0,
+};
 const VideoPlayer = ({ title, video }) => {
   document.title = title;
   return (
-    <div
-      className="container"
-      style={{
-        borderRadius: "15px",
-        aspectRatio: "16/9",
-      }}
-    >
-      <ReactPlayer
-        className="react-player"
-        url={video}
-        controls={true}
-        width="100%"
-        height="100%"
-      />
-    </div>
+    <>
+      <div
+        className="container"
+        style={{
+          borderRadius: "15px",
+          aspectRatio: "16/9",
+        }}
+      >
+        <ReactPlayer
+          className="react-player"
+          url={video}
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <Options />
+    </>
   );
 };
 VideoPlayer.propTypes = {
