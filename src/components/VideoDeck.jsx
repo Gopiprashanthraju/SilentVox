6f2726a231348652a13f2f9dc8c4c84b742a89f0
Gavini Sumanth <100990200+GaviniSumanth@users.noreply.github.com>
2023-11-25 05:45:48 +0000

    feat: add video list component

--- src/components/VideoDeck.jsx
+++ src/components/VideoDeck.jsx
@@ -1,11 +1,11 @@
-import { VideoCardMini } from "./VideoCard";
+import { VideoCard, VideoCardMini } from "./VideoCard";
 import PropTypes from "prop-types";
 
 export function VideoDeck({ title, videos }) {
   return (
     <>
       <div className="p-3">
-        <h1 className="fs-1">{title}</h1>
+        <h1 className="fs-1 text-white ">{title}</h1>
         <div
           style={{
             display: "grid",
@@ -45,7 +45,59 @@ VideoDeck.propTypes = {
 };
 VideoDeck.defaultProps = {
   title: "Video Deck",
-  videos: Array(10).fill({
+  videos: Array(8).fill({
+    title: "Dark Visions: The Elusive Video and the Galaxy's Shrouded Secrets",
+    thumbnail: "https://specializeddental.com/assets/placeholder-image.png",
+    creator: "DarkLordStrategy",
+    uri: "video-uri",
+  }),
+};
+export function VideoList({ videos }) {
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "minmax(300px, 1fr)",
+          gridGap: "0.1rem",
+          justifyContent: "center",
+          justifyItems: "center",
+          alignItems: "center",
+          alignContent: "center",
+        }}
+      >
+        {videos.map((video) => (
+          <div key={video.uri}>
+            <VideoCard
+              title={video.title}
+              description={video.description}
+              thumbnail={video.thumbnail}
+              creator={video.creator}
+              uri={video.uri}
+              views={video.views}
+              timestamp={video.timestamp}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+VideoList.propTypes = {
+  videos: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      thumbnail: PropTypes.string.isRequired,
+      creator: PropTypes.string.isRequired,
+      uri: PropTypes.string.isRequired,
+      views: PropTypes.number.isRequired,
+      timestamp: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+VideoList.defaultProps = {
+  videos: Array(4).fill({
     title: "Dark Visions: The Elusive Video and the Galaxy's Shrouded Secrets",
     thumbnail: "https://specializeddental.com/assets/placeholder-image.png",
     creator: "DarkLordStrategy",
